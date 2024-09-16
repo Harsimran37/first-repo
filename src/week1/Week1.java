65f7e8926b4c9178e41840be6a9f89e90c357779
Harsimran37 <kau16037@sheridancollege.ca>
2024-09-16 12:49:48 -0400

    initial commit - added a comment

---
+++ src/week1/Week1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week1;
+
+/**
+ *
+ * @author harsimran
+ * 
+ * 
+ */
+public class Week1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //added a comment
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
